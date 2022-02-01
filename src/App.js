7a5064d5674a3b42e0843cb9e3cd5da558072b62
leandro <martinezleadro2004@gmail.com>
2022-02-01 11:44:35 -0300

    resuelto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { cards } from './components/cards';
 
+// const themeStyles = {
+//   dark: {
+//     background: '#282c34',
+//     textColor: 'white'
+//   },
+//   light: {
+//     background: 'white',
+//     textColor: '#282c34'
+//   }
+// }
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? 'wrapper-dark' : 'wrapper-light'}>
+      
+      <div className="container"  >
+        <header className="header">
+          <div className="presentacion">
+            <h2>Hola, soy Leandro</h2>
+            <h4>Bienvenidos a mi blog!</h4>
+          </div>
+          <div>
+            <button type='button' onClick={()=> setDarkMode(!darkMode)} 
+            className='btn'>{darkMode ? 'Light Mode' : 'Dark Mode'}</button>
+          </div>
+        </header>
+      </div>
+    
+      <div className="primer-fondo">
+        <div className="info">
+          <h1>Lo mejor de la tecnología</h1>
+          <div className='boton'><button type="button" className={darkMode ? 'cto-dark' : 'cto-light'}>LO ENCONTRAS ACÁ</button></div>
+        </div>
+      </div>
+    
+      <div className="cards container">
+        {cards.map(cards => (
+          <div className='card'>
+            <div className='card-info'>
+              <h4>{cards.subtitulo}</h4>
+              <p>{cards.text}</p>
+            </div>
+          </div>         
+        ))}
+      </div>
+
+      <footer className="footer">
+        <p>Hola, soy Leandro | Todos los derechos reservados</p>
+      </footer>
+    
     </div>
   );
 }
---
+++ src/components/cards.js
@@ -0,0 +1,6 @@
+export const cards= [{subtitulo: 'Aprendiendo ReactJS', text: 'Una de las tecnologias con mayor demanda en el mercado'},
+{subtitulo: 'Aprendiendo ReactJS', text: 'Una de las tecnologias con mayor demanda en el mercado'},
+{subtitulo: 'Aprendiendo ReactJS', text: 'Una de las tecnologias con mayor demanda en el mercado'},
+{subtitulo: 'Aprendiendo ReactJS', text: 'Una de las tecnologias con mayor demanda en el mercado'},
+{subtitulo: 'Aprendiendo ReactJS', text: 'Una de las tecnologias con mayor demanda en el mercado'},
+{subtitulo: 'Aprendiendo ReactJS', text: 'Una de las tecnologias con mayor demanda en el mercado'}]
